63c29e4d1545b7cb2d1b4b6ba9f48b48c0bee605
Matteo <pro.matteo.piselli@gmail.com>
2025-05-28 22:16:32 +0200

    Réorganisation des imports et ajout de CORS dans app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var summonersRouter = require("./routes/summoner");
+var masteriesRouter = require("./routes/masteries");
 
 var app = express();
 
-app.use(logger('dev'));
+const cors = require("cors");
+app.use(cors());
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/summoner", summonersRouter);
+app.use("/masteries", masteriesRouter);
 
 module.exports = app;
