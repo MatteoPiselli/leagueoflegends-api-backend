08c0340049e0c117666f7ddc4dc443f4a338bb07
Matteo Piselli <pro.matteo.piselli@gmail.com>
2025-09-16 12:31:19 +0200

    Ajout de la fonction utilitaire pour récupérer les données de maîtrise des champions via PUUID

---
+++ api/masteriesApi.js
@@ -0,0 +1,32 @@
+// Utility functions to interact with Riot Games API for Champion Masteries
+
+const RIOT_API_KEY = process.env.RIOT_API_KEY;
+
+async function getMasteriesByPuuid(puuid) {
+  // --------------- Recovery of the player's masteries ------------------ //
+  const response = await fetch(
+    `https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/?api_key=${RIOT_API_KEY}`
+  );
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`Masteries error: ${response.status} - ${errorText}`);
+  }
+
+  // ------------- Recovery of the masteries data limiting to 3 champions ------------- //
+  const masteriesJson = await response.json();
+
+  const masteriesData = masteriesJson
+    ? masteriesJson.slice(0, 3).map((mastery) => ({
+        championId: mastery.championId,
+        championLevel: mastery.championLevel,
+        championPoints: mastery.championPoints,
+      }))
+    : [];
+
+  return masteriesData;
+}
+
+module.exports = {
+  getMasteriesByPuuid,
+};
