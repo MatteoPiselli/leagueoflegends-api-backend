f453ea25f9256e8cd2faeaba0f871ac0abec1b5b
Matteo Piselli <pro.matteo.piselli@gmail.com>
2025-09-16 12:31:47 +0200

    Refactor la route pour récupérer les maîtrises des champions en déléguant la logique au contrôleur

--- routes/masteries.js
+++ routes/masteries.js
@@ -1,48 +1,7 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const masteryController = require("../controllers/masteryController");
 
-const RIOT_API_KEY = process.env.RIOT_API_KEY;
-
-/* ----------------- Get masteries ------------- */
-router.get("/:puuid", async (req, res) => {
-  const { puuid } = req.params;
-
-  try {
-    if (!RIOT_API_KEY) {
-      return res.status(400).json({ error: "API KEY is required" });
-    }
-
-    // --------------- Recovery of the player’s masteries ------------------ //
-    const masteriesResponse = await fetch(
-      `https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/?api_key=${RIOT_API_KEY}`
-    );
-
-    if (!masteriesResponse.ok) {
-      const errorText = await masteriesResponse.text();
-      console.error("Erreur Masteries response:", errorText);
-      throw new Error(`Erreur Masteries response: ${masteriesResponse.status}`);
-    }
-
-    // ------------- Recovery of the masteries data limiting to 3 champions ------------- //
-    const masteriesJson = await masteriesResponse.json();
-
-    const masteriesData = masteriesJson
-      ? masteriesJson.slice(0, 3).map((mastery) => ({
-          championId: mastery.championId,
-          championLevel: mastery.championLevel,
-          championPoints: mastery.championPoints,
-        }))
-      : [];
-
-    res.json({ masteries: masteriesData });
-
-    // Catch errors
-  } catch (error) {
-    console.error("Error backend:", error.message);
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-});
+router.get("/:puuid", masteryController.getMasteries);
 
 module.exports = router;
