59710930018ad671a6298e2f0376948999f99c8a
Matteo Piselli <pro.matteo.piselli@gmail.com>
2025-09-16 11:36:43 +0200

    Correction de la réponse JSON pour renvoyer l'objet summoner au lieu de dbSummoner

--- controllers/summonerController.js
+++ controllers/summonerController.js
@@ -30,7 +30,7 @@ exports.searchSummoner = async (req, res) => {
     }
 
     // 4. Send info to client
-    res.json({ dbSummoner });
+    res.json({ summoner: dbSummoner });
   } catch (error) {
     // Centralized error handling
     console.error("Error backend :", error.message);
