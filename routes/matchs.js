8e9864fd7b657d5d7c1a96a66ea12b4987633795
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-09-30 19:24:48 +0200

    Correction du chemin d'importation du contrôleur des matchs pour utiliser le bon fichier

--- routes/matchs.js
+++ routes/matchs.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const matchController = require("../controllers/matchController");
+const matchController = require("../controllers/match");
 
 /* ----------------- Get Matchs History ------------- */
 router.get("/:puuid", matchController.getMatchHistory);
