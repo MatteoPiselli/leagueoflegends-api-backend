01255c9aebf429b25aba1fe1a57b1bc288d30b93
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-05 03:37:17 +0200

    Ajout de la vérification de l'option updateClicked pour ignorer la récupération des données de joueur si elles existent déjà dans la base de données

--- controllers/summonerController.js
+++ controllers/summonerController.js
@@ -8,16 +8,25 @@ const { getRiotId, getSummonerByPuuid } = require("../api/summonerApi");
  */
 exports.searchSummoner = async (req, res) => {
   const { username, tagline } = req.params;
+  const { updateClicked } = req.query;
 
   try {
     // 1. Get PUUID from Riot ID using utility function
     const riotIdData = await getRiotId(username, tagline);
     const puuid = riotIdData.puuid;
 
-    // 2. Get player info from PUUID using utility function
+    // 2. Check if we have summoner in database first (skip if updateClicked is true)
+    if (!updateClicked) {
+      const existingSummoner = await Summoner.findOne({ puuid });
+      if (existingSummoner) {
+        return res.json({ summoner: existingSummoner });
+      }
+    }
+
+    // 3. Get player info from PUUID using utility function
     const summonerData = await getSummonerByPuuid(puuid);
 
-    // 3. Create or update player in MongoDB
+    // 4. Create or update player in MongoDB
     const dbSummoner = await Summoner.findOneAndUpdate(
       { puuid },
       {
@@ -35,7 +44,7 @@ exports.searchSummoner = async (req, res) => {
       { new: true, upsert: true }
     );
 
-    // 4. Send info to client
+    // 5. Send info to client
     res.json({ summoner: dbSummoner });
   } catch (error) {
     const statusCode = error.statusCode || 500;
