5b7edb61dcf772547860c1ea78d68db492620e5c
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-05 03:37:25 +0200

    Ajout de la vérification de l'option updateClicked pour ignorer la récupération des données de match si elles existent déjà dans la base de données

--- controllers/match/getMatchHistory.js
+++ controllers/match/getMatchHistory.js
@@ -3,21 +3,24 @@ const { getMatchHistory } = require("../../api/matchApi");
 
 module.exports = async (req, res) => {
   const { puuid } = req.params;
+  const { updateClicked } = req.query;
 
   try {
-    // Check if we have matches in database first
-    const dbMatches = await Match.find({ "participants.puuid": puuid })
-      .sort({ gameCreation: -1 })
-      .limit(5)
-      .select("matchId");
+    // Check if we have matches in database first (skip if updateClicked is true)
+    if (!updateClicked) {
+      const dbMatches = await Match.find({ "participants.puuid": puuid })
+        .sort({ gameCreation: -1 })
+        .limit(5)
+        .select("matchId");
 
-    if (dbMatches.length > 0) {
-      // Return matches from database
-      const matchIds = dbMatches.map((match) => match.matchId);
-      return res.json({ matchs: matchIds });
+      if (dbMatches.length > 0) {
+        // Return matches from database
+        const matchIds = dbMatches.map((match) => match.matchId);
+        return res.json({ matchs: matchIds });
+      }
     }
 
-    // Get match history from Riot API if not enough in DB
+    // Get match history from Riot API
     const matchsHistoryData = await getMatchHistory(puuid, 0, 5);
 
     res.json({ matchs: matchsHistoryData });
