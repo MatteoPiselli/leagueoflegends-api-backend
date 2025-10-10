a497b8047e270c393ac03047580772267b1b3219
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-10-10 04:04:25 +0200

    Ajout de la connexion à la base de données avec Mongoose dans connection.js

--- database/connection.js
+++ database/connection.js
@@ -5,5 +5,13 @@ const connectionString = process.env.CONNECTION_STRING;
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
-
   .catch((error) => console.error(error));
+
+// Close the Mongoose connection when Ctrl+C is pressed
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("Database connection closed");
+  process.exit(0);
+});
+
+module.exports = mongoose;
